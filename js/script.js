30e2a373f29f3349440da5a971dfb44b1fdff3b1
DimancheHugo <144660052+DimancheHugo@users.noreply.github.com>
2023-11-09 12:07:53 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ const menu = document.getElementById('mobile-menu');
 const counters = document.querySelectorAll('.counter');
 let scrollStarted = false;
 
+
+
+
 btn.addEventListener('click', navToggle);
 document.addEventListener('scroll', scrollPage);
 
@@ -57,3 +60,165 @@ function countUp() {
 function reset() {
   counters.forEach((counter) => (counter.innerHTML = '0'));
 }
+let psswd = document.querySelector("#mdp");
+let psswdin = document.querySelector("#mdp");
+let inv = document.querySelectorAll("small");
+let invin = document.querySelectorAll("small");
+let n = document.querySelector("#nom");
+let nF = document.querySelector("#nomF");
+let admail = document.querySelector("#mail");
+let admailin = document.querySelector("#mail");
+let mess = document.querySelector("#Message");
+let age = document.querySelector("#age");
+let check = document.querySelector("#formCheck-1");
+let buup = document.querySelector(".btn.btn-primary");
+let buin = document.querySelector(".btn.btn-primary");
+buup.addEventListener("click",verifierFormulaireup);
+buin.addEventListener("click",verifierFormulairein);  
+
+
+function verifmdp(){
+  if(psswd.value.length <= 8 ){
+    psswd.classList.add("is-invalid");
+    inv[3].classList.remove("invisible");
+    psswd.classList.add("text-danger");
+    psswd.classList.remove("is-valid");
+  }else{
+    psswd.classList.add("is-valid");
+    psswd.classList.remove("is-invalid");
+    psswd.classList.remove("text-danger");
+    inv[3].classList.add("invisible");
+  }
+}
+
+function verifmdpin(){
+  if(psswd.value.length <= 8 ){
+    psswdin.classList.add("is-invalid");
+    inv[1].classList.remove("invisible");
+    psswdin.classList.add("text-danger");
+    psswdin.classList.remove("is-valid");
+  }else{
+    psswdin.classList.add("is-valid");
+    psswdin.classList.remove("is-invalid");
+    psswdin.classList.remove("text-danger");
+    inv[1].classList.add("invisible");
+  }
+}
+
+function verifname(){
+  if(n.value.length < 3 ){
+    n.classList.add("is-invalid");
+    inv[0].classList.remove("invisible");
+    n.classList.add("text-danger");
+    n.classList.remove("is-valid");
+  }else{
+    n.classList.add("is-valid");
+    inv[0].classList.add("invisible");
+    n.classList.remove("text-danger");
+    n.classList.remove("is-invalid");
+  }
+}
+
+function verifnameF(){
+  if(nF.value.length == 0 ){
+    nF.classList.add("is-invalid");
+    inv[1].classList.remove("invisible");
+    nF.classList.add("text-danger");
+    nF.classList.remove("is-valid");
+  }else{
+    nF.classList.add("is-valid");
+    inv[1].classList.add("invisible");
+    nF.classList.remove("text-danger");
+    nF.classList.remove("is-invalid");
+  }
+}
+
+function verifadmail(){
+  if(admail.value.length <3 ){
+    admail.classList.add("is-invalid");
+    inv[2].classList.remove("invisible");
+    admail.classList.add("text-danger");
+    admail.classList.remove("is-valid");
+  }else{
+    admail.classList.add("is-valid");
+    admail.classList.remove("is-invalid");
+    admail.classList.remove("text-danger");
+    inv[2].classList.add("invisible");
+  }
+}
+
+function verifadmailin(){
+  if(admail.value.length <3 ){
+    admailin.classList.add("is-invalid");
+    inv[0].classList.remove("invisible");
+    admailin.classList.add("text-danger");
+    admailin.classList.remove("is-valid");
+  }else{
+    admailin.classList.add("is-valid");
+    admailin.classList.remove("is-invalid");
+    admailin.classList.remove("text-danger");
+    inv[0].classList.add("invisible");
+  }
+}
+
+function verifmessage(){
+  if(mess.value.length == 0 ){
+    mess.classList.add("is-invalid");
+    inv[4].classList.remove("invisible");
+    mess.classList.add("text-danger");
+    mess.classList.remove("is-valid");
+  }else{
+    mess.classList.add("is-valid");
+    inv[4].classList.add("invisible");
+    mess.classList.remove("text-danger");
+    mess.classList.remove("is-invalid");
+  }
+}
+
+function verifage(){
+  if(check.checked === false ){
+    age.classList.add("nul");
+    age.classList.remove("bien");
+    inv[5].classList.remove("invisible");
+    age.classList.add("text-danger");
+  }else{
+    age.classList.add("bien");
+    age.classList.remove("nul");
+    inv[5].classList.add("invisible");
+    age.classList.remove("text-danger");
+  }
+}
+
+function verifierFormulaireup(){
+  verifmdp()
+  verifname()
+  verifnameF()
+  verifadmail()
+  verifmessage()
+  verifage()
+}
+
+function verifierFormulairein(){
+  verifmdpin()
+  verifadmailin()
+}
+// function verifmdp(){
+  //  for( let i = 0; i < i.length; i++){
+  //  if(i <= 8){
+    //  this.classList.add("text-danger")
+    // this.classList.add("is-invalid")
+  // }else{
+    // this.classList.add("is-valid")
+    // }
+  // }
+// }
+
+// let bu = document.querySelector(".btn.btn-primary");
+// bu.addEventListener("click",verifierFormulaire);
+
+// function verifierFormulaire(){
+    // verifmdp()
+
+// }
+ 
+
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-718a45dd9cf7e7f842a935f5ebbe5719a5e09af4491e668f4dbf3b35d5cca122.svg)](https://classroom.github.com/online_ide?assignment_repo_id=11824007&assignment_repo_type=AssignmentRepo)
+[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-718a45dd9cf7e7f842a935f5ebbe5719a5e09af4491e668f4dbf3b35d5cca122.svg)](https://classroom.github.com/online_ide?assignment_repo_id=12155992&assignment_repo_type=AssignmentRepo)
 Site de Running
 =======
 Cahier des charges
@@ -10,7 +10,7 @@ Les équipes de design ont élaboré une  (
 
 ![SpaceX Website](img/screen.jpg)
 
-Travail à réaliser
+Travail à réaliser Module 1
 -----------
 
  1. Page `index.html`,  ***DOM*** à compléter :
@@ -25,12 +25,23 @@ Travail à réaliser
     * Remplacer les `TODO` par les propriétès CSS  adéquates à l'aide de la charte graphique
     * Chercher des  images en rapport avec le thème du running que vous [redimensionnerez](https://www.resizepixel.com/)  pour respecter la charte graphique en `2600x1200`
 
-
 #### Liens Utiles
 
 - [Charte Graphique Plaquette Figma](https://www.figma.com/file/OYr51M2IjdeFcJpRFsFSgZ/spaceX?type=design&node-id=0%3A1&mode=design&t=silahs9HWIdfbBNO-1)
 
 - [Documentation sur le langage Emmet](https://docs.emmet.io/cheat-sheet/)
+
+Travail à réaliser Module 2
+-----------
+3. Formulaires de connexion  `Sign In` et d'inscription `Sign Up`
+  
+
+   
+   * **Visualiser** la capsule vidéo [manipuler le `DOM`](https://www.youtube.com/watch?v=iQ4_gQi6Xp4)  s'appuyant sur le site [ nouvelle-techno.fr ](https://nouvelle-techno.fr/articles/manipuler-le-dom-en-javascript).
+
+   * **Réaliser** [les exercices de découvertes](https://www.w3schools.com/js/exercise_js.asp?filename=exercise_js_dom_html1).
+
+   * **Coder** le fichier `script.js` permettant de respecter le [cahier des charges](https://drive.google.com/file/d/1g0I3di9m_nC00MlvVGNM4SoNElRQUjW1/view) pour les deux formulaires `signIn et signUp`.
 
 
 
